017bc2a3eb38cd247c8cf46f6f0e39e75bde5057
Geris <gerardo.a.l@hotmail.com>
2021-10-24 11:21:01 -0300

    add cors

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const router = express.Router();
 const sequelize = require("./database/dbconfig");
 
 require("./database/associations");
 
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({
